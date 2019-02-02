5982956b24822d45e49bddf097bda05e541727d1
salvo <salvo.cuccurullo@gmailcom>
2019-02-02 13:14:11 +0100

    adding UserDetailsService bean

--- MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/MongoApp.java
+++ MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/MongoApp.java
@@ -3,7 +3,12 @@ package com.salvocuccurullo.apps.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @SpringBootApplication
 @ComponentScan
@@ -12,5 +17,12 @@ public class MongoApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MongoApp.class, args);
     }
-}
 
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+}
