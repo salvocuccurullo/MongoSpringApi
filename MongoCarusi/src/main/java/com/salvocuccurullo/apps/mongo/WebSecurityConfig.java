35a1eed87fe758f911a04c48abf1d66d0f7b58ec
salvo <salvo.cuccurullo@gmailcom>
2019-02-03 00:06:18 +0100

    adding annotation WebSecurityConfig #4

--- MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/WebSecurityConfig.java
+++ MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/WebSecurityConfig.java
@@ -1,14 +1,10 @@
 package com.salvocuccurullo.apps.mongo;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +15,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .anyRequest().authenticated()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .and()
             .formLogin()
                 .and()
-            .httpBasic();
+            .httpBasic();        
     }
-    
+
 }
