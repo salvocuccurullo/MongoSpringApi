bb86b0635a5210d2d265e6c1577b79dde1864642
salvo <salvo.cuccurullo@gmailcom>
2019-02-02 13:28:33 +0100

    adding annotation @EnableWebSecurity

--- MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/MongoApp.java
+++ MongoCarusi/src/main/java/com/salvocuccurullo/apps/mongo/MongoApp.java
@@ -3,26 +3,16 @@ package com.salvocuccurullo.apps.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
 @ComponentScan
+@EnableWebSecurity
 public class MongoApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MongoApp.class, args);
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() throws Exception {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
-        return manager;
-    }
-
 }
